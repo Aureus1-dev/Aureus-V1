4911f58bdb114e5fd5fcf04a30731ba1a4e4d433
Aureus1-dev <aureus.quatise@gmail.com>
2026-08-14 10:53:34 -0400

    PF-008: export corrections panel

--- apps/web/design-system/components/business-console/index.ts
+++ apps/web/design-system/components/business-console/index.ts
@@ -1,3 +1,4 @@
 export { BusinessConsoleHome } from './BusinessConsoleHome';
 export { BusinessKnowledgeWorkspace } from './BusinessKnowledgeWorkspace';
 export { BusinessOperationsPanel } from './BusinessOperationsPanel';
+export { BusinessCorrectionsPanel } from './BusinessCorrectionsPanel';
