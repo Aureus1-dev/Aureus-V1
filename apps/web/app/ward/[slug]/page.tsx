db206526c1ca5b72376eb68546380ab0a295fdd9
Aureus1-dev <aureus.quatise@gmail.com>
2026-08-14 12:58:03 -0400

    PF-009 surface governed remodel intake

--- apps/web/app/ward/[slug]/page.tsx
+++ apps/web/app/ward/[slug]/page.tsx
@@ -1,6 +1,12 @@
 import { PublicWardExperience } from '../../../design-system/components/public-ward';
+import { KitchenBathIntakePanel } from '../../../design-system/components/public-ward/KitchenBathIntakePanel';
 
 export default async function PublicWardPage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
-  return <PublicWardExperience slug={slug} />;
+  return (
+    <>
+      <PublicWardExperience slug={slug} />
+      <KitchenBathIntakePanel slug={slug} />
+    </>
+  );
 }
