79f7709dadeebe1dd530fb9f90f61f8de06ea0fa
Aureus1-dev <aureus.quatise@gmail.com>
2026-08-14 10:53:42 -0400

    PF-008: mount reviewed corrections

--- apps/web/app/(member)/business/knowledge/page.tsx
+++ apps/web/app/(member)/business/knowledge/page.tsx
@@ -1,5 +1,13 @@
-import { BusinessKnowledgeWorkspace } from '../../../../design-system/components/business-console';
+import {
+  BusinessCorrectionsPanel,
+  BusinessKnowledgeWorkspace,
+} from '../../../../design-system/components/business-console';
 
 export default function BusinessKnowledgePage() {
-  return <BusinessKnowledgeWorkspace />;
+  return (
+    <>
+      <BusinessKnowledgeWorkspace />
+      <BusinessCorrectionsPanel />
+    </>
+  );
 }
