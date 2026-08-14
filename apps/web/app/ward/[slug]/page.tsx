4b11fabe40c8729ed32506e348c0a221c6560cfd
Aureus1-dev <aureus.quatise@gmail.com>
2026-08-14 14:11:39 -0400

    PF-011 accept SMS continuation on Ward page

--- apps/web/app/ward/[slug]/page.tsx
+++ apps/web/app/ward/[slug]/page.tsx
@@ -1,11 +1,19 @@
-import { PublicWardExperience } from '../../../design-system/components/public-ward';
 import { KitchenBathIntakePanel } from '../../../design-system/components/public-ward/KitchenBathIntakePanel';
+import { PublicWardContinuationGate } from '../../../design-system/components/public-ward/PublicWardContinuationGate';
 
-export default async function PublicWardPage({ params }: { params: Promise<{ slug: string }> }) {
+export default async function PublicWardPage({
+  params,
+  searchParams,
+}: {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ continue?: string | string[] }>;
+}) {
   const { slug } = await params;
+  const query = await searchParams;
+  const continuationToken = typeof query.continue === 'string' ? query.continue : undefined;
   return (
     <>
-      <PublicWardExperience slug={slug} />
+      <PublicWardContinuationGate slug={slug} continuationToken={continuationToken} />
       <KitchenBathIntakePanel slug={slug} />
     </>
   );
